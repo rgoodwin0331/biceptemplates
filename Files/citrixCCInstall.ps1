3df418f974d49b6f4356be51dd4db94000bd7229
rgoodwin0331 <95768052+rgoodwin0331@users.noreply.github.com>
2023-10-31 15:16:45 -0400

    Update citrixCCInstall.ps1
    Signed-off-by: rgoodwin0331 <95768052+rgoodwin0331@users.noreply.github.com>

--- Files/citrixCCInstall.ps1
+++ Files/citrixCCInstall.ps1
@@ -1,3 +1,5 @@
 Invoke-WebRequest -Uri "https://downloads.cloud.com/x1ojiluounsw/connector/cwcconnector.exe" -OutFile "c:\temp\cwcconnector.exe"
 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rgoodwin0331/biceptemplates/main/Files/cloudConnect.json" -OutFile "c:\temp\cloudConnect.json"
+
+cmd /c "c:\temp\cwcconnector.exe /q /ParametersFilePath:c:\temp\cloudConnect.json"
